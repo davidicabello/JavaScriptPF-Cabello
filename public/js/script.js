d38bf99b5e9ce5cbd7dcec79a75ed111e1d41aa9
David Cabello <davidivan.eda@gmail.com>
2023-06-08 18:57:18 -0300

    better js

--- public/js/script.js
+++ public/js/script.js
@@ -1,107 +1,239 @@
 // const scramble = require('@ignatiusmb/scramble')
 // scramble(document.querySelector('#yourElement')).run() // single element
+//Scrambler for brand logo
+// const TEXTS = [
+//     'Umbral ®',
+//     'Cyber Security',
+//     'Digital-First',
+//     'Design Agency'
+
+// ];
+// const scrambler = new window.Scrambler();
+// const scramblerTwo = new window.Scrambler();
+// const scramblerThree = new window.Scrambler();
+// const scramblerFour = new window.Scrambler();
+// const handleScramble = (text) => {
+//     document.getElementById('scrambletext').innerHTML = text;
+// };
+// let i = 0;
+// let scramblingTimeout;
+// function printText() {
+//     scrambler.scramble(TEXTS[i % TEXTS.length], handleScramble);
+//     i++;
+//     scramblingTimeout = setTimeout(printText, 3500);
+// };
+// printText();
+// //Scrambler for TRABAJO
+// const TEXTSTWO = [
+//     'Trabajo'
+// ];
+// const handleScrambleTwo = (text) => {
+//     document.getElementById('work-scrambler').innerHTML = text;
+// };
+// let z = 0;
+// let scramblingTimeoutTwo;
+// function printTextTwo() {
+//     scramblerTwo.scramble(TEXTSTWO[z % TEXTSTWO.length], handleScrambleTwo);
+//     z++;
+//     scramblingTimeoutTwo = setTimeout(printTextTwo, 50);
+// };
+
+// function startScrambling() {
+//     printTextTwo(); // Start scrambling when hovering over the word
+// };
+// function stopScrambling() {
+//     clearTimeout(scramblingTimeoutTwo); // Stop scrambling when hovering away from the word
+// };
+// //Scrambler for AGENCIA
+// const TEXTSTHREE = [
+//     'Agencia'
+// ];
+// const handleScrambleThree = (text) => {
+//     document.getElementById('agencia-scrambler').innerHTML = text;
+// };
+// let x = 0;
+// let scramblingTimeoutThree;
+// function printTextThree() {
+//     scramblerThree.scramble(TEXTSTHREE[x % TEXTSTHREE.length], handleScrambleThree);
+//     x++;
+//     scramblingTimeoutThree = setTimeout(printTextThree, 50);
+// };
+// function startScramblingTwo() {
+//     printTextThree(); // Start scrambling when hovering over the word
+// };
+// function stopScramblingTwo() {
+//     clearTimeout(scramblingTimeoutThree); // Stop scrambling when hovering away from the word
+// };
+
+// //Scrambler for SERVICIOS
+// const TEXTSFOUR = [
+//     'Servicios'
+// ];
+// const handleScrambleFour = (text) => {
+//     document.getElementById('servicios-scrambler').innerHTML = text;
+// };
+// let p = 0;
+// let scramblingTimeoutFour;
+// function printTextFour() {
+//     scramblerThree.scramble(TEXTSFOUR[p % TEXTSFOUR.length], handleScrambleFour);
+//     p++;
+//     scramblingTimeoutFour = setTimeout(printTextFour, 50);
+// };
+// function startScramblingThree() {
+//     printTextFour(); // Start scrambling when hovering over the word
+// };
+// function stopScramblingThree() {
+//     clearTimeout(scramblingTimeoutFour); // Stop scrambling when hovering away from the word
+// };
+
+// //Scrambler for CLIENTES
+// const TEXTSFIVE = [
+//     'Clientes'
+// ];
+// const handleScrambleFive = (text) => {
+//     document.getElementById('clientes-scrambler').innerHTML = text;
+// };
+// let o = 0;
+// let scramblingTimeoutFive;
+// function printTextFive() {
+//     scramblerThree.scramble(TEXTSFIVE[o % TEXTSFIVE.length], handleScrambleFive);
+//     o++;
+//     scramblingTimeoutFive = setTimeout(printTextFive, 50);
+// };
+// function startScramblingFour() {
+//     printTextFive(); // Start scrambling when hovering over the word
+// };
+// function stopScramblingFour() {
+//     clearTimeout(scramblingTimeoutFive); // Stop scrambling when hovering away from the word
+// };
 
 const TEXTS = [
     'Umbral ®',
     'Cyber Security',
     'Digital-First',
     'Design Agency'
-
 ];
 const scrambler = new window.Scrambler();
-const scramblerTwo = new window.Scrambler();
-const scramblerThree = new window.Scrambler();
-const scramblerFour = new window.Scrambler();
-const handleScramble = (text) => {
-    document.getElementById('scrambletext').innerHTML = text;
-};
 let i = 0;
 let scramblingTimeout;
+
 function printText() {
+    const handleScramble = (text) => {
+        document.getElementById('scrambletext').innerHTML = text;
+    };
+
     scrambler.scramble(TEXTS[i % TEXTS.length], handleScramble);
     i++;
     scramblingTimeout = setTimeout(printText, 3500);
-};
+}
+
 printText();
 
-const TEXTSTWO = [
-    'Trabajo'
-];
-const handleScrambleTwo = (text) => {
-    document.getElementById('work-scrambler').innerHTML = text;
-};
+const TEXTSTWO = ['Trabajo'];
+const scramblerTwo = new window.Scrambler();
 let z = 0;
 let scramblingTimeoutTwo;
+
 function printTextTwo() {
+    const handleScrambleTwo = (text) => {
+        document.getElementById('work-scrambler').innerHTML = text;
+    };
+
     scramblerTwo.scramble(TEXTSTWO[z % TEXTSTWO.length], handleScrambleTwo);
     z++;
-    scramblingTimeoutTwo = setTimeout(printTextTwo, 3500);
-};
+    scramblingTimeoutTwo = setTimeout(printTextTwo, 50);
+}
 
 function startScrambling() {
     printTextTwo(); // Start scrambling when hovering over the word
-};
+}
+
 function stopScrambling() {
     clearTimeout(scramblingTimeoutTwo); // Stop scrambling when hovering away from the word
-};
+}
 
-const TEXTSTHREE = [
-    'Agencia'
-];
-const handleScrambleThree = (text) => {
-    document.getElementById('agencia-scrambler').innerHTML = text;
-};
+const TEXTSTHREE = ['Agencia'];
+const scramblerThree = new window.Scrambler();
 let x = 0;
 let scramblingTimeoutThree;
+
 function printTextThree() {
+    const handleScrambleThree = (text) => {
+        document.getElementById('agencia-scrambler').innerHTML = text;
+    };
+
     scramblerThree.scramble(TEXTSTHREE[x % TEXTSTHREE.length], handleScrambleThree);
     x++;
-    scramblingTimeoutThree = setTimeout(printTextThree, 3500);
-};
+    scramblingTimeoutThree = setTimeout(printTextThree, 50);
+}
+
 function startScramblingTwo() {
     printTextThree(); // Start scrambling when hovering over the word
-};
+}
+
 function stopScramblingTwo() {
     clearTimeout(scramblingTimeoutThree); // Stop scrambling when hovering away from the word
-};
-const TEXTSFOUR = [
-    'Servicios'
-];
-const handleScrambleFour = (text) => {
-    document.getElementById('servicios-scrambler').innerHTML = text;
-};
+}
+
+const TEXTSFOUR = ['Servicios'];
+const scramblerFour = new window.Scrambler();
 let p = 0;
 let scramblingTimeoutFour;
+
 function printTextFour() {
-    scramblerThree.scramble(TEXTSFOUR[p % TEXTSFOUR.length], handleScrambleFour);
+    const handleScrambleFour = (text) => {
+        document.getElementById('servicios-scrambler').innerHTML = text;
+    };
+
+    scramblerFour.scramble(TEXTSFOUR[p % TEXTSFOUR.length], handleScrambleFour);
     p++;
-    scramblingTimeoutFour = setTimeout(printTextFour, 3500);
-};
+    scramblingTimeoutFour = setTimeout(printTextFour, 50);
+}
+
 function startScramblingThree() {
     printTextFour(); // Start scrambling when hovering over the word
-};
+}
+
 function stopScramblingThree() {
     clearTimeout(scramblingTimeoutFour); // Stop scrambling when hovering away from the word
-};
-const TEXTSFIVE = [
-    'Clientes'
-];
-const handleScrambleFive = (text) => {
-    document.getElementById('clientes-scrambler').innerHTML = text;
-};
+}
+
+const TEXTSFIVE = ['Clientes'];
+const scramblerFive = new window.Scrambler();
 let o = 0;
 let scramblingTimeoutFive;
+
 function printTextFive() {
-    scramblerThree.scramble(TEXTSFIVE[o % TEXTSFIVE.length], handleScrambleFive);
+    const handleScrambleFive = (text) => {
+        document.getElementById('clientes-scrambler').innerHTML = text;
+    };
+
+    scramblerFive.scramble(TEXTSFIVE[o % TEXTSFIVE.length], handleScrambleFive);
     o++;
-    scramblingTimeoutFive = setTimeout(printTextFive, 3500);
-};
+    scramblingTimeoutFive = setTimeout(printTextFive, 50);
+}
+
 function startScramblingFour() {
     printTextFive(); // Start scrambling when hovering over the word
-};
+}
+
 function stopScramblingFour() {
     clearTimeout(scramblingTimeoutFive); // Stop scrambling when hovering away from the word
-};
+}
+
+// Event listeners for hovering
+document.getElementById('work-scrambler').addEventListener('mouseover', startScrambling);
+document.getElementById('work-scrambler').addEventListener('mouseout', stopScrambling);
+
+document.getElementById('agencia-scrambler').addEventListener('mouseover', startScramblingTwo);
+document.getElementById('agencia-scrambler').addEventListener('mouseout', stopScramblingTwo);
+
+document.getElementById('servicios-scrambler').addEventListener('mouseover', startScramblingThree);
+document.getElementById('servicios-scrambler').addEventListener('mouseout', stopScramblingThree);
+
+document.getElementById('clientes-scrambler').addEventListener('mouseover', startScramblingFour);
+document.getElementById('clientes-scrambler').addEventListener('mouseout', stopScramblingFour);
+
 AOS.init();
 
 
