d5a92dacc617a0a06c5e6092ac1da952d9dce5ed
David Cabello <davidivan.eda@gmail.com>
2023-06-25 11:43:34 -0300

    updates

--- public/js/script.js
+++ public/js/script.js
@@ -1,112 +1,4 @@
-// const scramble = require('@ignatiusmb/scramble')
-// scramble(document.querySelector('#yourElement')).run() // single element
-//Scrambler for brand logo
-// const TEXTS = [
-//     'Umbral ®',
-//     'Cyber Security',
-//     'Digital-First',
-//     'Design Agency'
-
-// ];
-// const scrambler = new window.Scrambler();
-// const scramblerTwo = new window.Scrambler();
-// const scramblerThree = new window.Scrambler();
-// const scramblerFour = new window.Scrambler();
-// const handleScramble = (text) => {
-//     document.getElementById('scrambletext').innerHTML = text;
-// };
-// let i = 0;
-// let scramblingTimeout;
-// function printText() {
-//     scrambler.scramble(TEXTS[i % TEXTS.length], handleScramble);
-//     i++;
-//     scramblingTimeout = setTimeout(printText, 3500);
-// };
-// printText();
-// //Scrambler for TRABAJO
-// const TEXTSTWO = [
-//     'Trabajo'
-// ];
-// const handleScrambleTwo = (text) => {
-//     document.getElementById('work-scrambler').innerHTML = text;
-// };
-// let z = 0;
-// let scramblingTimeoutTwo;
-// function printTextTwo() {
-//     scramblerTwo.scramble(TEXTSTWO[z % TEXTSTWO.length], handleScrambleTwo);
-//     z++;
-//     scramblingTimeoutTwo = setTimeout(printTextTwo, 50);
-// };
-
-// function startScrambling() {
-//     printTextTwo(); // Start scrambling when hovering over the word
-// };
-// function stopScrambling() {
-//     clearTimeout(scramblingTimeoutTwo); // Stop scrambling when hovering away from the word
-// };
-// //Scrambler for AGENCIA
-// const TEXTSTHREE = [
-//     'Agencia'
-// ];
-// const handleScrambleThree = (text) => {
-//     document.getElementById('agencia-scrambler').innerHTML = text;
-// };
-// let x = 0;
-// let scramblingTimeoutThree;
-// function printTextThree() {
-//     scramblerThree.scramble(TEXTSTHREE[x % TEXTSTHREE.length], handleScrambleThree);
-//     x++;
-//     scramblingTimeoutThree = setTimeout(printTextThree, 50);
-// };
-// function startScramblingTwo() {
-//     printTextThree(); // Start scrambling when hovering over the word
-// };
-// function stopScramblingTwo() {
-//     clearTimeout(scramblingTimeoutThree); // Stop scrambling when hovering away from the word
-// };
-
-// //Scrambler for SERVICIOS
-// const TEXTSFOUR = [
-//     'Servicios'
-// ];
-// const handleScrambleFour = (text) => {
-//     document.getElementById('servicios-scrambler').innerHTML = text;
-// };
-// let p = 0;
-// let scramblingTimeoutFour;
-// function printTextFour() {
-//     scramblerThree.scramble(TEXTSFOUR[p % TEXTSFOUR.length], handleScrambleFour);
-//     p++;
-//     scramblingTimeoutFour = setTimeout(printTextFour, 50);
-// };
-// function startScramblingThree() {
-//     printTextFour(); // Start scrambling when hovering over the word
-// };
-// function stopScramblingThree() {
-//     clearTimeout(scramblingTimeoutFour); // Stop scrambling when hovering away from the word
-// };
-
-// //Scrambler for CLIENTES
-// const TEXTSFIVE = [
-//     'Clientes'
-// ];
-// const handleScrambleFive = (text) => {
-//     document.getElementById('clientes-scrambler').innerHTML = text;
-// };
-// let o = 0;
-// let scramblingTimeoutFive;
-// function printTextFive() {
-//     scramblerThree.scramble(TEXTSFIVE[o % TEXTSFIVE.length], handleScrambleFive);
-//     o++;
-//     scramblingTimeoutFive = setTimeout(printTextFive, 50);
-// };
-// function startScramblingFour() {
-//     printTextFive(); // Start scrambling when hovering over the word
-// };
-// function stopScramblingFour() {
-//     clearTimeout(scramblingTimeoutFive); // Stop scrambling when hovering away from the word
-// };
-
+//SCRAMBLE TEXT FUNCTIONS
 const TEXTS = [
     'Umbral ®',
     'Cyber Security',
@@ -233,6 +125,85 @@ document.getElementById('servicios-scrambler').addEventListener('mouseout', stop
 
 document.getElementById('clientes-scrambler').addEventListener('mouseover', startScramblingFour);
 document.getElementById('clientes-scrambler').addEventListener('mouseout', stopScramblingFour);
+
+
+//ADVICE GENERATOR
+
+const resDiv = document.querySelector('.results');
+const resBtn = document.querySelector('#data');
+const advNumbDiv = document.querySelector('.advice-number');
+
+
+resBtn.addEventListener('click', () => {
+    getAdvice();
+});
+
+window.onload = () => {
+    getAdvice();
+};
+
+
+
+function getAdvice() {
+
+    fetch('	https://api.adviceslip.com/advice').then(response => {
+        return response.json();
+    }).then(adviceData => {
+        const Adviceobj = adviceData.slip;
+        resDiv.innerHTML = `<h1><q>${Adviceobj.advice}</h1>`;
+        advNumbDiv.innerHTML = `<p>ADVICE # ${Adviceobj.id}</p>`;
+    }).catch(error => {
+        console.log(error);
+    });
+
+}
+
+//SWEET ALERT 
+const sweetAlertSuccess = document.getElementById('swallSuccess').addEventListener('click', swallSuccess);
+
+function swallSuccess() {
+    swal.fire({
+        title: "Exito!",
+        text: "Mensaje enviado!",
+        icon: "success"
+    })
+};
+
+//BOOTSTRAP VALIDATOR
+
+(() => {
+    'use strict'
+    const forms = document.querySelectorAll('.needs-validation')
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
+//EXCHANGE RATE JQUERY
+$(document).ready(function () {
+    $("#calcular-btn").click(function () {
+        let presupuesto = $("#presupuesto-input").val();
+        $.ajax({
+            url: "https://api.bluelytics.com.ar/v2/latest",
+            method: "GET",
+        })
+            .done(function (data) {
+                let blueDolar = data.blue.value_buy;
+                let totalArs = presupuesto;
+                let totalUsd = totalArs / blueDolar;
+                $("#presupuesto").html("La conversión de : $" + totalArs + " pesos a dólares al cambio del dia es u$s " + totalUsd);
+
+            });
+    });
+});
+
+
 
 AOS.init();
 
